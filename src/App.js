6fe4bcd528fe15f1f2c9c6be28052f0d352510d3
Margot <margaretsobota@gmail.com>
2021-02-05 15:53:13 -0600

    yarn add firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import logo from "./images/logoLIJ.png";
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVRzOlafcGV_2jfKdOJOfx4yFHI6pHORE",
+  authDomain: "drj-app-4012a.firebaseapp.com",
+  databaseURL: "https://drj-app-4012a-default-rtdb.firebaseio.com",
+  projectId: "drj-app-4012a",
+  storageBucket: "drj-app-4012a.appspot.com",
+  messagingSenderId: "841250660386",
+  appId: "1:841250660386:web:e980109a53b6ef4fa1aa7b",
+  measurementId: "G-LMS8MD7H9S"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 function App() {
   return (
