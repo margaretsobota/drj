4a1bf11def1a2db7613d4c96cf030552168977a5
Margot <margaretsobota@gmail.com>
2021-02-12 12:58:55 -0600

    trying to add steps state

--- src/components/StageForm.js
+++ src/components/StageForm.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { Box, Container, Button} from "@material-ui/core";
 import StepForm from "./StepForm";
 import { makeStyles } from '@material-ui/core/styles';
@@ -15,13 +16,34 @@ const useStyles = makeStyles((theme) => ({
 
 const StageForm = ({ stage }) => {
   const styles = useStyles();
+  const newStep = {
+    title: "",
+    description: ""
+  };
+  const [stepsState, setSteps] = useState([newStep]);
+
+  const returnSteps = () => {
+    const steps = stepsState.map((step) =>
+      <StepForm />
+    );
+    return (
+      <Box>
+        {steps}
+      </Box>
+    )
+  };
+
+  const addStep = () => {
+    const newSteps = stepsState.push(newStep);
+    setSteps(newSteps);
+  }
 
   return (
     <Container className={styles.stageContainer}>
       <h2>
         {stage}
       </h2>
-      <StepForm />
+      {returnSteps()}
       <Container>
         <Button
           className={styles.newButton}
