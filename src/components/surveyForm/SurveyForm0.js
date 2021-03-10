31ca3dabb40daff7b488a5abe6e0fde22f1ad942
Margot <margaretsobota@gmail.com>
2021-03-09 18:41:18 -0600

    design done

--- src/components/surveyForm/SurveyForm0.js
+++ src/components/surveyForm/SurveyForm0.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Button, Container, MenuItem, Select, InputLabel } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  MenuItem,
+  Select,
+  InputLabel
+} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import "firebase/database";
 import _ from "lodash";
-import {
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 
@@ -17,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "row",
     flexWrap: "wrap",
     maxWidth: '70%',
-
+    marginTop: "40px"
   },
   inputContainer: {
     background: "rgba(163, 217, 209, 0.2)",
@@ -25,19 +29,34 @@ const useStyles = makeStyles((theme) => ({
     padding: "40px",
     paddingBottom: "100px",
     margin: "10px",
-    flex: 3
+    flex: 3,
+    height: "300px"
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
-  saveButton: {
-    justifyContent: "center",
-    padding: "10px",
-    margin: "80px",
-    background: "#699BF7",
-    color: "#FFFFFF",
+  buttonLink: {
+    textDecoration: "none",
+    color: "#005AFF",
+    textTransform: "none",
+    fontFamily: "Roboto",
+    fontSize: "18px",
+    lineHeight: "36px",
+    fontWeight: "400"
+  },
+  nextButton: {
+    padding: "10px 35px 10px 35px",
+    border: "1px solid #699BF7",
+    float: "right"
+  },
+  label: {
+    fontFamily: "Roboto",
+    fontSize: "19px",
+    lineHeight: "31px",
+    fontWeight: "400",
+    color: "#2A303D",
+    marginBottom: "20px"
   }
-
 }));
 
 const SurveyForm0 = ({demographicState}) => {
@@ -54,67 +73,61 @@ const SurveyForm0 = ({demographicState}) => {
   return (
     <Container className={styles.formControl}>
       <Container className={styles.inputContainer}>
-      <InputLabel>Gender</InputLabel>
-      <Select
-        required
-        name="gender"
-        onChange={handleSave}
-        style={{width:'200px'}}
-      >
-        <MenuItem value={"male"}>Male</MenuItem>
-        <MenuItem value={"female"}>Female</MenuItem>
-        <MenuItem value={"other"}>Other</MenuItem>
+        <InputLabel className={styles.label}>
+          1. What is your gender?
+        </InputLabel>
+        <Select
+          required
+          name="gender"
+          onChange={handleSave}
+          style={{width:'200px'}}
+        >
+          <MenuItem value={"male"}>Male</MenuItem>
+          <MenuItem value={"female"}>Female</MenuItem>
+          <MenuItem value={"other"}>Other</MenuItem>
 
-      </Select>
+        </Select>
       </Container>
-
       <Container className={styles.inputContainer}>
-      <InputLabel>Age</InputLabel>
-      <Select
-        name="age"
-        onChange={handleSave}
-        style={{width:'200px'}}
-      >
-        <MenuItem value={0}>Under 18</MenuItem>
-        <MenuItem value={1}>18-24</MenuItem>
-        <MenuItem value={2}>25-34</MenuItem>
-        <MenuItem value={3}>35-44</MenuItem>
-        <MenuItem value={4}>45-54</MenuItem>
-        <MenuItem value={5}>55-64</MenuItem>
-        <MenuItem value={6}>65+</MenuItem>
-      </Select>
+        <InputLabel className={styles.label}>2. What is your age?</InputLabel>
+        <Select
+          name="age"
+          onChange={handleSave}
+          style={{width:'200px'}}
+        >
+          <MenuItem value={0}>Under 18</MenuItem>
+          <MenuItem value={1}>18-24</MenuItem>
+          <MenuItem value={2}>25-34</MenuItem>
+          <MenuItem value={3}>35-44</MenuItem>
+          <MenuItem value={4}>45-54</MenuItem>
+          <MenuItem value={5}>55-64</MenuItem>
+          <MenuItem value={6}>65+</MenuItem>
+        </Select>
       </Container>
-
       <Container className={styles.inputContainer}>
-      <InputLabel id="income">Annual Household Income</InputLabel>
-      <Select
-        name="income"
-        onChange={handleSave}
-        style={{width:'200px'}}
-      >
-        <MenuItem value={0}>RD$416,000 or below</MenuItem>
-        <MenuItem value={1}>RD$416,000-624,000</MenuItem>
-        <MenuItem value={2}>RD$624,000-827,000</MenuItem>
-        <MenuItem value={3}>RD$867,000 or greater</MenuItem>
-      </Select>
+        <InputLabel className={styles.label} id="income">
+          3. What is your annual household income?
+        </InputLabel>
+        <Select
+          name="income"
+          onChange={handleSave}
+          style={{width:'200px'}}
+        >
+          <MenuItem value={0}>RD$416,000 or below</MenuItem>
+          <MenuItem value={1}>RD$416,000-624,000</MenuItem>
+          <MenuItem value={2}>RD$624,000-827,000</MenuItem>
+          <MenuItem value={3}>RD$867,000 or greater</MenuItem>
+        </Select>
       </Container>
-      <Container align="center">
-          <Link to="/surveyForm1">
-          <Button
-            type="primary"
-            style={{
-              border: "1px solid #699BF7",
-              background:"#699BF7",
-              boxSizing: "border-box",
-              borderRadius: "5px",
-              marginTop:"40px",
-              padding: "20px"
-            }}
-          >
-
+      <Container align="left" style={{paddingLeft: "10px", marginTop: "100px"}}>
+        <Button
+          type="primary"
+          className={styles.nextButton}
+        >
+          <Link className={styles.buttonLink} to="/surveyForm1">
             Next
-          </Button>
           </Link>
+        </Button>
       </Container>
   </Container>
 )};
--- src/components/surveyForm/SurveyForm1.js
+++ src/components/surveyForm/SurveyForm1.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Button, Container, TextField, MenuItem, Select, InputLabel, FormControl, FormHelperText } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  TextField,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormHelperText
+} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import "firebase/database";
 import _ from "lodash";
-import {
-  Link
-} from "react-router-dom";
-
-
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -16,26 +20,45 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "row",
     flexWrap: "wrap",
     maxWidth: '70%',
-
+    marginTop: "40px"
   },
   inputContainer: {
     background: "rgba(163, 217, 209, 0.2)",
     padding: "40px",
     paddingBottom: "100px",
     margin: "10px",
-    flex: 3
+    flex: 3,
+    height: "300px"
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
-  saveButton: {
-    justifyContent: "center",
-    padding: "10px",
-    margin: "80px",
-    background: "#699BF7",
-    color: "#FFFFFF",
+  buttonLink: {
+    textDecoration: "none",
+    color: "#005AFF",
+    textTransform: "none",
+    fontFamily: "Roboto",
+    fontSize: "18px",
+    lineHeight: "36px",
+    fontWeight: "400"
+  },
+  prevButton: {
+    padding: "10px 35px 10px 35px",
+    border: "1px solid #699BF7"
+  },
+  nextButton: {
+    padding: "10px 35px 10px 35px",
+    border: "1px solid #699BF7",
+    float: "right"
+  },
+  label: {
+    fontFamily: "Roboto",
+    fontSize: "19px",
+    lineHeight: "31px",
+    fontWeight: "400",
+    color: "#2A303D",
+    marginBottom: "20px"
   }
-
 }));
 
 const SurveyForm1 = ({demographicState}) => {
@@ -51,85 +74,72 @@ const SurveyForm1 = ({demographicState}) => {
   return (
     <Container className={styles.formControl}>
       <Container className={styles.inputContainer}>
-      <FormControl>
-      <InputLabel>Education Level</InputLabel>
-      <Select
-        name="education"
-        onChange={handleSave}
-        style={{width:'200px'}}
-      >
-        <MenuItem value={"preescolar"}>Nivel preescolar</MenuItem>
-        <MenuItem value={"primaria"}>Nivel primaria</MenuItem>
-        <MenuItem value={"secundaria"}>Nivel secundaria</MenuItem>
-        <MenuItem value={"superior"}>Nivel superior</MenuItem>
-      </Select>
-      <FormHelperText>Please select your highest achieved level of education</FormHelperText>
-      </FormControl>
-
+        <InputLabel className={styles.label}>
+          4. What is your education level?
+        </InputLabel>
+        <Select
+          name="education"
+          onChange={handleSave}
+          style={{width:'200px'}}
+        >
+          <MenuItem value={"preescolar"}>Nivel preescolar</MenuItem>
+          <MenuItem value={"primaria"}>Nivel primaria</MenuItem>
+          <MenuItem value={"secundaria"}>Nivel secundaria</MenuItem>
+          <MenuItem value={"superior"}>Nivel superior</MenuItem>
+        </Select>
+        <FormHelperText>
+          Please select your highest achieved level of education
+        </FormHelperText>
       </Container>
       <Container className={styles.inputContainer}>
-      <InputLabel>City of Residence</InputLabel>
-      <TextField
-        name="city"
-        multiline
-        variant="outlined"
-        onChange={handleSave}
-        style={{width:'200px'}}
-      >
-      </TextField>
+        <InputLabel className={styles.label}>
+          5. What is your city of residence?
+        </InputLabel>
+        <TextField
+          name="city"
+          multiline
+          variant="outlined"
+          onChange={handleSave}
+          style={{width:'200px'}}
+        >
+        </TextField>
       </Container>
-
       <Container className={styles.inputContainer}>
-      <FormControl required>
-      <InputLabel >Kilometers Traveled</InputLabel>
-      <Select
-        name="distance"
-        onChange={handleSave}
-        style={{width:'200px'}}
-
-      >
-        <MenuItem value={0}>Under 10km</MenuItem>
-        <MenuItem value={1}>11km-19km</MenuItem>
-        <MenuItem value={2}>20km-29km</MenuItem>
-        <MenuItem value={3}>30km-39km</MenuItem>
-        <MenuItem value={4}>40km-49km</MenuItem>
-        <MenuItem value={5}>Over 50km</MenuItem>
-      </Select>
-      <FormHelperText>Please indicate approximately how long it took you to travel to court</FormHelperText>
-      </FormControl>
+        <InputLabel className={styles.label}>
+          6. How many kilometers did you travel?
+        </InputLabel>
+        <Select
+          name="distance"
+          onChange={handleSave}
+          style={{width:'200px'}}
+        >
+          <MenuItem value={0}>Under 10km</MenuItem>
+          <MenuItem value={1}>11km-19km</MenuItem>
+          <MenuItem value={2}>20km-29km</MenuItem>
+          <MenuItem value={3}>30km-39km</MenuItem>
+          <MenuItem value={4}>40km-49km</MenuItem>
+          <MenuItem value={5}>Over 50km</MenuItem>
+        </Select>
+        <FormHelperText>Please indicate approximately how long it took you to travel to court</FormHelperText>
       </Container>
-      <Container align="center">
-          <Link to="/surveyForm0">
-          <Button type="primary"
-            style={{
-              background: "#FFFFFF",
-              border: "1px solid #699BF7",
-              boxSizing: "border-box",
-              borderRadius: "5px",
-              marginRight: "20px",
-              marginTop:"40px",
-              padding: "20px"
-            }}
-          >
+      <Container align="left" style={{paddingLeft: "10px", marginTop: "100px"}}>
+        <Button
+          type="primary"
+          className={styles.prevButton}
+        >
+          <Link className={styles.buttonLink} to="/surveyForm0">
             Previous
-          </Button>
           </Link>
-          <Link to="/surveyForm2">
-          <Button type="primary"
-            style={{
-              border: "1px solid #699BF7",
-              boxSizing: "border-box",
-              background:"#699BF7",
-              borderRadius: "5px",
-              marginTop:"40px",
-              padding: "20px"
-            }}
-          >
+        </Button>
+        <Button
+          type="primary"
+          className={styles.nextButton}
+        >
+          <Link className={styles.buttonLink} to="/surveyForm2">
             Next
-          </Button>
           </Link>
+        </Button>
       </Container>
-
   </Container>
 )};
 
