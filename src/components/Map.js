7cc306d7589d3a01c32a510ecb4e14cdd2b8c083
Margot <margaretsobota@gmail.com>
2021-03-16 12:35:04 -0500

    button not included

--- src/components/Map.js
+++ src/components/Map.js
@@ -365,208 +365,210 @@ const Map = ({mapRef}) => {
   }
 
   return (
-    <Box
-      id="container"
-      component="div"
-      style={{
-        paddingLeft:"10px",
-        fontFamily: "Roboto"
-      }}
-    >
-      <Title
-        title="Journey Map"
-        subtitle="Porttitor mattis nulla justo commodo at maecenas porta, eu ultricies ut."
-      />
-      <svg id="containerSVG" width="1500" height="750">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          y={starOffset}
-          viewBox="0 0 24 24"
-          fill="#2A303D"
-        >
-          <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/>
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          y={starOffset + stepsBarHeight/2-30}
-          viewBox="0 0 24 24"
-          fill="#2A303D"
-        >
-           <path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524v-12.005zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/>
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          y={starOffset + stepsBarHeight-30}
-          viewBox="0 0 24 24"
-          fill="#2A303D"
-        >
-          <path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524-4.721 2.525.942-5.27-3.861-3.71 5.305-.733 2.335-4.817zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/>
-        </svg>
-        <svg
-          width="26"
-          height={stepsBarHeight}
-          fill="none"
-          viewBox="0 0 26 481"
-          xmlns="http://www.w3.org/2000/svg"
-          x={30}
-          y={100}
-        >
-          <path d="M24.5088 0.673065H12.7495M0.990234 0.673065H12.7495M12.7495 0.673065V479.673H0.990234H24.5088" stroke="black" stroke-linecap="round"/>
-        </svg>
-        <rect width="1390" height="680" fill="#FCF6EC" x={60} y={14} />
-        <Bar
-          x={labelOffset}
-          y={24}
-          width={40}
-          height={60}
-          fill={"#F2F2F2"}
+    <Box>
+      <Box
+        id="container"
+        component="div"
+        style={{
+          paddingLeft:"10px",
+          fontFamily: "Roboto"
+        }}
+      >
+        <Title
+          title="Journey Map"
+          subtitle="Porttitor mattis nulla justo commodo at maecenas porta, eu ultricies ut."
         />
-        <Text
-          x={labelOffset + 20}
-          y={54}
-          fill={"#303031"}
-          angle={270}
-          textAnchor="middle"
-          verticalAnchor="middle"
-          className={styles.labelText}
-        >
-          PHASES
-        </Text>
-        <Bar
-          x={labelOffset}
-          y={100}
-          width={40}
-          height={stepsBarHeight}
-          fill={"#F2F2F2"}
-        />
-        <Text
-          x={labelOffset + 20}
-          y={stepsBarHeight/2 + 85}
-          fill={"#303031"}
-          angle={270}
-          textAnchor="middle"
-          verticalAnchor="middle"
-          className={styles.labelText}
-        >
-          STEPS
-        </Text>
-        <Bar
-          x={labelOffset}
-          y={600}
-          width={40}
-          height={80}
-          fill={"#F2F2F2"}
-        />
-        <Text
-          x={labelOffset + 20}
-          y={640}
-          fill={"#303031"}
-          angle={270}
-          textAnchor="middle"
-          verticalAnchor="middle"
-          className={styles.labelText}
-        >
-          BACKEND
-        </Text>
-        {
-          phaseColumns.map((phase, i) => {
-            return (
-              <Group
-                key={`phaseColumn-${i}`}
-                top="10"
-                left="10"
-              >
-                <Bar
-                  x={(i * 190) + leftOffset}
-                  y={14}
-                  width={barWidth}
-                  height={60}
-                  fill={phase.fill}
-                />
-                <Text
-                  x={(i * 190) + leftOffset + barWidth/2}
-                  y="50"
-                  fill={i < 2 ? "#FCF6EC": "#303031"}
-                  style={{
-                    fontSize: "19.8925px",
-                    fontFamily: "Roboto",
-                    fontStyle: "normal",
-                    fontWeight: "bold"
-                  }}
-                  textAnchor="middle"
+        <svg id="containerSVG" width="1500" height="750">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            y={starOffset}
+            viewBox="0 0 24 24"
+            fill="#2A303D"
+          >
+            <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/>
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            y={starOffset + stepsBarHeight/2-30}
+            viewBox="0 0 24 24"
+            fill="#2A303D"
+          >
+             <path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524v-12.005zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/>
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            y={starOffset + stepsBarHeight-30}
+            viewBox="0 0 24 24"
+            fill="#2A303D"
+          >
+            <path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524-4.721 2.525.942-5.27-3.861-3.71 5.305-.733 2.335-4.817zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/>
+          </svg>
+          <svg
+            width="26"
+            height={stepsBarHeight}
+            fill="none"
+            viewBox="0 0 26 481"
+            xmlns="http://www.w3.org/2000/svg"
+            x={30}
+            y={100}
+          >
+            <path d="M24.5088 0.673065H12.7495M0.990234 0.673065H12.7495M12.7495 0.673065V479.673H0.990234H24.5088" stroke="black" stroke-linecap="round"/>
+          </svg>
+          <rect width="1390" height="680" fill="#FCF6EC" x={60} y={14} />
+          <Bar
+            x={labelOffset}
+            y={24}
+            width={40}
+            height={60}
+            fill={"#F2F2F2"}
+          />
+          <Text
+            x={labelOffset + 20}
+            y={54}
+            fill={"#303031"}
+            angle={270}
+            textAnchor="middle"
+            verticalAnchor="middle"
+            className={styles.labelText}
+          >
+            PHASES
+          </Text>
+          <Bar
+            x={labelOffset}
+            y={100}
+            width={40}
+            height={stepsBarHeight}
+            fill={"#F2F2F2"}
+          />
+          <Text
+            x={labelOffset + 20}
+            y={stepsBarHeight/2 + 85}
+            fill={"#303031"}
+            angle={270}
+            textAnchor="middle"
+            verticalAnchor="middle"
+            className={styles.labelText}
+          >
+            STEPS
+          </Text>
+          <Bar
+            x={labelOffset}
+            y={600}
+            width={40}
+            height={80}
+            fill={"#F2F2F2"}
+          />
+          <Text
+            x={labelOffset + 20}
+            y={640}
+            fill={"#303031"}
+            angle={270}
+            textAnchor="middle"
+            verticalAnchor="middle"
+            className={styles.labelText}
+          >
+            BACKEND
+          </Text>
+          {
+            phaseColumns.map((phase, i) => {
+              return (
+                <Group
+                  key={`phaseColumn-${i}`}
+                  top="10"
+                  left="10"
                 >
-                  {phase.text}
-                </Text>
-              </Group>
-            )
-          })
-        }
-        <LinePath
-          curve={allCurves[curveType]}
-          data={mapData}
-          stroke="rgba(33,18,20,0.5)"
-          strokeWidth={2}
-          shapeRendering="geometricPrecision"
-          markerMid="url(#marker-circle)"
-        />
-        {
-          mapData.map((p) => {
-            const pointIndex = mapData.indexOf(p);
-            return (
-              <Group>
-                <Text
-                  x={p[0] - 10}
-                  y={p[1] - 30}
-                  fill={"#303031"}
-                  style={{
-                    fontFamily: "Roboto",
-                    fontWeight: "300",
-                    fontSize: "12px",
-                    lineHeight: "11px"
-                  }}
-                >
-                  {stepMapFinal[pointIndex].title}
-                </Text>
-                <circle
-                  fill="#242055"
-                  id={"circle" + pointIndex}
-                  r={18}
-                  cx={p[0]}
-                  cy={p[1]}
-                  stroke="rgba(42,48,61,0.5)"
-                  onMouseEnter={handleHover}
-                  onMouseLeave={handleHoverEnd}
-                />
-                {getIcon(pointIndex, p)}
-                <Text
-                  id={"description" + pointIndex}
-                  x={p[0] - 10}
-                  y={p[1] + 30}
-                  fill={"#303031"}
-                  style={{
-                    fontFamily: "Roboto",
-                    fontWeight: "300",
-                    fontSize: "12px",
-                    lineHeight: "11px",
-                    visibility: "hidden",
-                    background:"#FFFFF"
-                  }}
-                  width={175}
-                  verticalAnchor={"start"}
-                >
-                    {stepMapFinal[pointIndex].description}
+                  <Bar
+                    x={(i * 190) + leftOffset}
+                    y={14}
+                    width={barWidth}
+                    height={60}
+                    fill={phase.fill}
+                  />
+                  <Text
+                    x={(i * 190) + leftOffset + barWidth/2}
+                    y="50"
+                    fill={i < 2 ? "#FCF6EC": "#303031"}
+                    style={{
+                      fontSize: "19.8925px",
+                      fontFamily: "Roboto",
+                      fontStyle: "normal",
+                      fontWeight: "bold"
+                    }}
+                    textAnchor="middle"
+                  >
+                    {phase.text}
                   </Text>
-              </Group>
-            )
-          })
-        }
-      </svg>
+                </Group>
+              )
+            })
+          }
+          <LinePath
+            curve={allCurves[curveType]}
+            data={mapData}
+            stroke="rgba(33,18,20,0.5)"
+            strokeWidth={2}
+            shapeRendering="geometricPrecision"
+            markerMid="url(#marker-circle)"
+          />
+          {
+            mapData.map((p) => {
+              const pointIndex = mapData.indexOf(p);
+              return (
+                <Group>
+                  <Text
+                    x={p[0] - 10}
+                    y={p[1] - 30}
+                    fill={"#303031"}
+                    style={{
+                      fontFamily: "Roboto",
+                      fontWeight: "300",
+                      fontSize: "12px",
+                      lineHeight: "11px"
+                    }}
+                  >
+                    {stepMapFinal[pointIndex].title}
+                  </Text>
+                  <circle
+                    fill="#242055"
+                    id={"circle" + pointIndex}
+                    r={18}
+                    cx={p[0]}
+                    cy={p[1]}
+                    stroke="rgba(42,48,61,0.5)"
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleHoverEnd}
+                  />
+                  {getIcon(pointIndex, p)}
+                  <Text
+                    id={"description" + pointIndex}
+                    x={p[0] - 10}
+                    y={p[1] + 30}
+                    fill={"#303031"}
+                    style={{
+                      fontFamily: "Roboto",
+                      fontWeight: "300",
+                      fontSize: "12px",
+                      lineHeight: "11px",
+                      visibility: "hidden",
+                      background:"#FFFFF"
+                    }}
+                    width={175}
+                    verticalAnchor={"start"}
+                  >
+                      {stepMapFinal[pointIndex].description}
+                    </Text>
+                </Group>
+              )
+            })
+          }
+        </svg>
+      </Box>
       <Button
         className={styles.downloadButton}
         onClick = {handleDownload}
